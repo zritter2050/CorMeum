c4ad82b68462783cbb24952cc4df30988fca58b3
zritter2050 <78902866+zritter2050@users.noreply.github.com>
2023-10-28 00:41:50 +0200

    Add files via upload

--- hrf.py
+++ hrf.py
@@ -7,19 +7,35 @@ import joblib
 import streamlit as st
 
 # %%
+import shap
+import time
+from shap import summary_plot
+from shap import TreeExplainer, Explanation
+import streamlit.components.v1 as components
+
+# %%
 filename='RandomForest_model_ts_20.pkl'
 model = open(filename, 'rb')
 rf_model=joblib.load(model)
 
 # %%
-def rf_prediction(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall):
+def rf_pred_prob(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall):
+    columns=['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg',
+         'thalachh', 'exng', 'oldpeak', 'slp', 'caa', 'thall']
     pred_arr=np.array([age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall])
-    preds=pred_arr.reshape(1,-1)
+    preds=pd.DataFrame(data=pred_arr.reshape(1,-1), columns=columns)
     model_prediction=rf_model.predict(preds)
-    return model_prediction    
+    model_prediction_prob=rf_model.predict_proba(preds)
+    return model_prediction, model_prediction_prob, preds    
+
+# %%
+def st_shap(plot, height=None):
+    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+    components.html(shap_html, height=height)
 
 # %%
 def run():
+
     st.title('CorMeum')
     st.title('Model for Heart Attack Prediction (RFM)')
     st.text('Author: Dr. Z. Ritter')
@@ -58,10 +74,34 @@ def run():
     thall=st.text_input("on thalassemia blood disorder (1 = normal, 2 = fixed defect, 3 = reversable defect)")
     
     prediction=""
+    probability=""
+    preds_pd=pd.DataFrame()
+    explainer=shap.TreeExplainer(rf_model)  # explainer
+    shap_values =list()
+    i=0
+    
     if st.button("Predict"):
-        prediction=rf_prediction(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall)
-    st.success("Prediction: if your risk score is [0], it means you are not at risk of having a heart attack, and that's good news!, if it is [1] consult a specialist. Your risk value is: {}".format( prediction ))  
+        i=1
+        prediction=rf_pred_prob(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall)[0]
+        probability=rf_pred_prob(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall)[1][0][1]*100
+        preds_pd=rf_pred_prob(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall)[2]
+        
+        shap.initjs()
+        shap_values = explainer.shap_values(preds_pd)
+        
 
+
+    st.success("Prediction: if your risk score is [0], it means you are not at risk of having a heart attack, and that's good news!, if it is [1] consult a specialist. Your risk value is: {}".format( prediction ))  
+    st.success("Probability: Your probability risk value in % is: {}".format( probability ))
+    st.subheader('your data at once :')
+    st.write(preds_pd)  
+    st.info('Probability values: 0, class 1 or not risk attack and probabilities for each parameter.  1: class 2, or having a risk attack and probabilites for each parameter')
+    st.write(shap_values)
+    #st.write(len(shap_values))
+    #st.write(i)
+    st.write('Explainability (XAI): in pink are the parameters that increase the predicted probability risk, in blue those that decrease its value. In bold is the probability value of having a heart attack, as shown here:')
+    if i==1:
+        st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1], preds_pd))
 
 # %%
 if __name__=='__main__':
